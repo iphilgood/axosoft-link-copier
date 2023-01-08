c1b537b1fa900037cd68d1cef22ec2d9117c644d
Phil Schilter <phil.schilter@gmail.com>
2023-01-08 11:58:01 +0100

    fix logo icon

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,11 @@
 <template>
-  <a class="icon-btn mx-2 text-2xl" rel="noreferrer" href="https://github.com/antfu/vitesse-webext" target="_blank" title="GitHub">
-    <pixelarticons-power />
+  <a
+    class="icon-btn mx-2 text-2xl"
+    rel="noreferrer"
+    href="https://github.com/iphilgood/axosoft-link-copier"
+    target="_blank"
+    title="GitHub"
+  >
+    <pixelarticons-link class="text-axo-dark" />
   </a>
 </template>
